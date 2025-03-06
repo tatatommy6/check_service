2b7927b15fea01a741527d2d9f947325a538513f
rrayy <rrayy25809@gmail.com>
2025-03-06 20:42:06 +0900

    Changes to be committed: db 설치 	modified:   main.py 	new file:   templates/index.html 	new file:   user.db

---
+++ main.py
@@ -0,0 +1,27 @@
+import flask
+from flask import render_template, request, redirect, url_for, session
+import sqlite3
+import json
+
+app = flask.Flask(__name__)
+conn = sqlite3.connect('user.db', check_same_thread=False)
+cursor = conn.cursor()
+flask.secret_key = 'LN$oaYB9-5KBT7G'
+
+#SQLite 데이터베이스 초기화
+def init_db():
+    cursor.execute('''
+        CREATE TABLE IF NOT EXISTS users (
+            "number"	INTEGER,
+        );
+    ''')
+    conn.commit()
+    conn.close()
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    #init_db()
+    app.run(debug=True, host='0.0.0.0')
