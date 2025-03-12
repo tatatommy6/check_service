2e6daa72eb76eb5f372521abe6c983d066a53582
rrayy <rrayy25809@gmail.com>
2025-03-12 14:41:47 +0900

    Changes to be committed: 네비게이션 바 구현 	modified:   static/Logo.png 	deleted:    static/main.js 	new file:   static/navbar.js 	modified:   static/style.css 	modified:   templates/index.html 	modified:   templates/login.html

---
+++ static/navbar.js
@@ -0,0 +1,24 @@
+let navbar = document.getElementsByClassName('navbar')[0];
+navbar.innerHTML = `<div class="container-fluid">
+            <a class="navbar-brand" href="#"><img src="static/Logo.png" alt="EduTrack Logo" height="40"></a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+                aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="/">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Features</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="/login">Login</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link disabled" aria-disabled="true">Disabled</a>
+                    </li>
+                </ul>
+            </div>
+        </div>`;
