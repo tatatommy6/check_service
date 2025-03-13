aee4267d61fc8bf201dd7e68c109a9957284298a
rrayy <rrayy25809@gmail.com>
2025-03-13 20:43:31 +0900

    Changes to be committed: 마이페이지 qr 코드 생성 	modified:   main.py 	new file:   qr_code_with_logo.png 	deleted:    qrcode.py 	modified:   static/qrcode.js 	modified:   templates/mypage.html

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import flask
-from flask import render_template, request, redirect, url_for, session
+from flask import render_template, request, redirect, send_file, url_for, session
 import sqlite3
-import json
+import qrcode
+from PIL import Image
+from io import BytesIO
 import db_manager
 
 app = flask.Flask(__name__)
@@ -24,6 +26,31 @@ def login():
             return '로그인 실패', 401
     else:
         return render_template('login.html')
+    
+@app.route('/mypage')
+def mypage():
+    return render_template('mypage.html')
+    
+@app.route('/generate_qr', methods=['POST'])
+def generate_qr():
+    #number = session['number']
+    # QR 코드 이미지 생성
+    qr = qrcode.QRCode(
+        version=1,
+        error_correction=qrcode.constants.ERROR_CORRECT_L,
+        box_size=10,
+        border=4,
+    )
+    qr.add_data("Hello, World!")
+    qr.make(fit=True)
+    # 이미지 저장 (BytesIO)
+    img = qr.make_image(fill="black", back_color="white")
+    img_bytes = BytesIO()
+    img.save(img_bytes, format="PNG")
+    img_bytes.seek(0)
+
+    # 이미지 바이너리 반환
+    return send_file(img_bytes, mimetype="image/png")
 
 if __name__ == '__main__':
     db_manager.conn = sqlite3.connect('user.db', check_same_thread=False)
--- static/qrcode.js
+++ static/qrcode.js
@@ -1,14 +1,15 @@
-async function getQRCode(data) {
-    const response = await fetch(`/generate_qr/?data=${encodeURIComponent(data)}`);
-    const blob = await response.blob();
-
-    // Blob을 URL로 변환하여 이미지 표시
-    const imgUrl = URL.createObjectURL(blob);
-    document.getElementById("qrImage").src = imgUrl;
-}
-
 // 버튼 클릭 시 실행
-document.getElementById("generateBtn").addEventListener("click", () => {
-    const inputData = document.getElementById("qrData").value;
-    getQRCode(inputData);
+document.getElementById("generateBtn").addEventListener("click", async () => {
+    const response = await fetch("/generate_qr", {
+        method: "POST"
+    });
+    if (response.ok) {
+        const blob = await response.blob();
+
+        // Blob을 URL로 변환하여 이미지 표시
+        const imgUrl = URL.createObjectURL(blob);
+        document.getElementById("qrImage").src = imgUrl;
+    } else {
+        alert("QR 코드 생성에 실패했습니다.");
+    }
 });
